3a5a6b1baabca849f80ce2aec21500dc4cc7498e
Kirill <kirrilf@mail.ru>
2020-04-15 00:48:24 +0300

    first commit

---
+++ weatherApp/views.py
@@ -0,0 +1,47 @@
+from django.shortcuts import render
+import requests
+from .forms import CityForm
+
+def index(request):
+  
+
+    api_url = "http://api.openweathermap.org/data/2.5/weather"
+
+    city = "London"
+
+
+
+    form = CityForm(request.POST or None)
+    if request.method == 'POST':
+        if form.is_valid():
+            city = form.cleaned_data.get("city")
+
+    params = {
+        'q': city,
+        'appid':'11c0d3dc6093f7442898ee49d2430d20',
+        'units': 'metric'
+    } 
+    context = {
+        'form':form,
+        'errorInput':False
+    }   
+
+    res = requests.get(api_url, params=params).json()
+    if(res["cod"] == 200):
+
+        info = {
+                'city':city,
+                'temp':res["main"]["temp"],
+                'icon':res["weather"][0]["icon"],
+                'pressure':res["main"]["pressure"],
+                'humidity':res["main"]["humidity"],
+                'windSpeed':res["wind"]["speed"]
+        }
+        context['info'] = info
+    else:
+        context["errorInput"] = True
+
+
+
+    print(context["errorInput"])
+    return render(request, 'weather/show.html', context)
